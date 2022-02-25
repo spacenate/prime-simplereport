ff7f7980f220603a5b852a4b0d768c8d9f5d2ef1
Zedd Shmais <zedd@skylight.digital>
2022-02-25 09:40:22 -0600

    add random test_orders

--- backend/src/main/java/gov/cdc/usds/simplereport/service/RandomDataController.java
+++ backend/src/main/java/gov/cdc/usds/simplereport/service/RandomDataController.java
@@ -10,13 +10,14 @@ import gov.cdc.usds.simplereport.db.model.auxiliary.TestResult;
 import gov.cdc.usds.simplereport.db.repository.DeviceSpecimenTypeRepository;
 import gov.cdc.usds.simplereport.db.repository.FacilityRepository;
 import gov.cdc.usds.simplereport.db.repository.PatientAnswersRepository;
-import gov.cdc.usds.simplereport.db.repository.TestEventRepository;
 import gov.cdc.usds.simplereport.db.repository.TestOrderRepository;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -26,7 +27,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RandomDataController {
   private final TestOrderRepository testOrderRepository;
-  private final TestEventRepository testEventRepository;
   private final FacilityRepository facilityRepository;
   private final PersonService personService;
   private final DeviceSpecimenTypeRepository deviceSpecimenTypeRepository;
@@ -37,12 +37,14 @@ public class RandomDataController {
   private final Random random = new Random();
 
   @GetMapping("/generateTests")
-  public String generateRandomData(@RequestParam UUID personId, @RequestParam UUID facilityId) {
+  public String generateRandomData(@RequestParam UUID personId, @RequestParam UUID facilityId)
+      throws ExecutionException, InterruptedException {
     generateRandomTests(personId, facilityId);
     return "Done!";
   }
 
-  public void generateRandomTests(UUID personId, UUID facilityId) {
+  public void generateRandomTests(UUID personId, UUID facilityId)
+      throws ExecutionException, InterruptedException {
     Person person = personService.getPatientNoPermissionsCheck(personId);
     Facility facility = facilityRepository.findById(facilityId).get();
     List<DeviceSpecimenType> dsts = deviceSpecimenTypeRepository.findAll();
@@ -52,27 +54,32 @@ public class RandomDataController {
     patientAnswersRepository.save(askOnEntrySurvey);
 
     for (int i = 0; i < 1000; i++) {
-      long ms = -946771200000L + (Math.abs(random.nextLong()) % (70L * 365 * 24 * 60 * 60 * 1000));
-      Date randomDate = new Date(ms);
-      TestResult randomResult =
-          results.get((random.nextInt() & Integer.MAX_VALUE) % results.size());
-      DeviceSpecimenType randomDST = dsts.get((random.nextInt() & Integer.MAX_VALUE) % dsts.size());
-
-      TestOrder order = new TestOrder(person, facility);
-
-      order.setAskOnEntrySurvey(askOnEntrySurvey);
-      order.setDeviceSpecimen(randomDST);
-      order.setResult(randomResult);
-      order.setDateTestedBackdate(randomDate);
-      order.markComplete();
-
-      order = testOrderRepository.save(order);
-
-      //      TestEvent testEvent = new TestEvent(order, false);
-      //      testEventRepository.save(testEvent);
-      //
-      //      order.setTestEventRef(testEvent);
-      //      order = testOrderRepository.save(order);
+      ArrayList<TestOrder> orders = new ArrayList<>();
+      for (int j = 0; j < 1000; j++) {
+        orders.add(addTestOrder(person, facility, dsts, askOnEntrySurvey));
+      }
+      testOrderRepository.saveAll(orders);
     }
+  }
+
+  private TestOrder addTestOrder(
+      Person person,
+      Facility facility,
+      List<DeviceSpecimenType> dsts,
+      PatientAnswers askOnEntrySurvey) {
+    long ms = -946771200000L + (Math.abs(random.nextLong()) % (70L * 365 * 24 * 60 * 60 * 1000));
+    Date randomDate = new Date(ms);
+    TestResult randomResult = results.get((random.nextInt() & Integer.MAX_VALUE) % results.size());
+    DeviceSpecimenType randomDST = dsts.get((random.nextInt() & Integer.MAX_VALUE) % dsts.size());
+
+    TestOrder order = new TestOrder(person, facility);
+
+    order.setAskOnEntrySurvey(askOnEntrySurvey);
+    order.setDeviceSpecimen(randomDST);
+    order.setResult(randomResult);
+    order.setDateTestedBackdate(randomDate);
+    order.markComplete();
+
+    return order;
   }
 }
