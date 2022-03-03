553e6bf373e064d5a14c5aabf1a88f14a77e9ef9
Zedd Shmais <zedd@skylight.digital>
2022-03-03 15:43:20 -0600

    upgrade to spring 2.6.6
    renable github caching
    
    removing gradle cache
    
    logging standard out
    
    show running tests in ci
    
    try using any as the checksum
    
    updating checksum for azure test
    
    changed migration to specify the schemas
    
    fixed demo okta temp org access
    
    fixed getUser graphql query
    
    fix auditFailure_noDataReturned test
    
    remove unused contexts from tests
    
    add back dev + sl4j dependencies
    
    cleaned up code
    
    reverted okta dependencies to older version
    
    reset azure insights versions to before
    
    reverting db changes for now
    
    generate only 1 jar
    
    make the linters god happy
    
    getting local okta to work
    
    updating okta dependencies to match new spring version
    
    update migration sql to include schema name as result of liquibase core version change from 3.10 to 4.5
    
    Add OktaLocalCorsSecurityConfiguration.java
    
    removed CurrentTenantDataAccessContextHolder usages
    
    removed CurrentTenantDataAccessContextHolder usages
    
    most tests are passing
    
    wip

--- backend/src/test/java/gov/cdc/usds/simplereport/api/graphql/TestResultTest.java
+++ backend/src/test/java/gov/cdc/usds/simplereport/api/graphql/TestResultTest.java
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import gov.cdc.usds.simplereport.api.CurrentTenantDataAccessContextHolder;
 import gov.cdc.usds.simplereport.api.model.Role;
 import gov.cdc.usds.simplereport.db.model.DeviceType;
 import gov.cdc.usds.simplereport.db.model.Facility;
@@ -38,7 +37,6 @@ class TestResultTest extends BaseGraphqlTest {
   @Autowired private TestDataFactory _dataFactory;
   @Autowired private OrganizationService _orgService;
   @MockBean private SmsService _smsService;
-  @MockBean private CurrentTenantDataAccessContextHolder _tenantDataAccessContextHolder;
 
   private Organization _org;
   private Facility _site;
@@ -252,7 +250,7 @@ class TestResultTest extends BaseGraphqlTest {
         "organization-level-metrics",
         "GetOrganizationLevelDashboardMetrics",
         variables,
-        "Current user does not have permission to request [/organizationLevelDashboardMetrics]");
+        "Current user does not have permission for this action");
   }
 
   @Test
@@ -311,7 +309,7 @@ class TestResultTest extends BaseGraphqlTest {
         "dashboard-metrics",
         "GetTopLevelDashboardMetrics",
         variables,
-        "Current user does not have permission to request [/topLevelDashboardMetrics]");
+        "Current user does not have permission for this action");
   }
 
   private ObjectNode getFacilityScopedArguments() {
